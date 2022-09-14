f4242aa2082679a4e83fde39a7499d446c8150a0
christianamaury <chris.amaury@hotmail.com>
2022-09-13 19:40:44 -0700

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {About} from "./components/About";
+import {Nav} from "./components/Nav";
+import {Shop} from "./components/Shop";
+// To Handle Routing; BrowserRouter
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//About will only show whenever I go to the page
 
 function App() {
   return (
+
+<Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Nav/>
+     <Switch>
+       <Route path="/" component={Home} />
+       <Route path="/about" component={About} />
+       <Route path="/shop" component={Shop} />
+     </Switch>
     </div>
+</Router>
   );
 }
+
+const Home = () => (
+
+  <div>
+    <h1> Home Page </h1>
+  </div>
+
+);
 
 export default App;
---
+++ src/components/Shop.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import '../App.css';
+
+export class Shop extends Component{
+
+  render(){
+    return (
+          <div>
+            <h1> Shop Page </h1>
+          </div>
+    )
+  }
+}
